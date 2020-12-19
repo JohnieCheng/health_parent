553e47d4fe12e7d5a4d1e6c0b2b6446667f756f3
“e <1793792999@qq.com>
2020-12-19 15:55:30 +0800

    程建衡第一次提交

---
+++ health_mobile/src/main/java/com/itheima/controller/AddressManageController.java
@@ -0,0 +1,28 @@
+package com.itheima.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.itheima.constant.MessageConstant;
+import com.itheima.entity.PageResult;
+import com.itheima.entity.QueryPageBean;
+import com.itheima.entity.Result;
+import com.itheima.pojo.Address;
+import com.itheima.service.AddressManageService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/addressManage")
+public class AddressManageController {
+
+    @Reference
+    private AddressManageService addressManageService;
+
+    @RequestMapping("/findAll")
+    public List<Address> findAll() {
+        return addressManageService.findAll();
+    }
+}
